da438fbc846d441f8ef69585280d716d8968ae28
Abdul Basit <abdulbasitbasit076@gmail.com>
2026-01-10 15:11:22 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,8 +16,8 @@ const Footer = () => {
 
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2025{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
+          © 2025
+          <a href="" className="hover:underline">
             AbdulBasit™
           </a>
           . All Rights Reserved.
